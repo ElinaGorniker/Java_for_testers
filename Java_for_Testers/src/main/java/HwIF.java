116b86bd3b8d89c3a01ca845d5b62b2ccc382b07
elinagorniker <elinagorniker@gmail.com>
2023-03-07 14:36:56 +0100

    <Homework If and Switch>

---
+++ Java_for_Testers/src/main/java/HwIF.java
@@ -0,0 +1,29 @@
+public class HwIF {
+    public static void main(String[] args) {
+        int intNumber;
+        intNumber = 0;
+
+        if (intNumber < 0) {
+              System.out.println("Negative");
+
+        }else {
+            if (intNumber == 0) {
+                System.out.println("Zero");
+            } else {
+                if ((intNumber > 0) && (intNumber < 10))  {
+                    System.out.println("Small");
+                }else {
+                    if ((intNumber > 10) && (intNumber < 100)) {
+                        System.out.println("Less then 100");
+                    } else {
+                        if (intNumber > 100) {
+                            System.out.println("Too big");
+                        } else {
+                            if (intNumber == 23) {
+                                System.out.println("Jordan");
+                            }
+                        }
+                    }}}}}}
+
+
+
